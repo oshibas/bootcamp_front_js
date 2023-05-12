e9cb8775eac6d7a69f19511b60de9cb35161ab8f
oshibas <saoritsushima@gmail.com>
2023-05-12 21:09:35 +0900

    ook

--- src/ts/application.ts
+++ src/ts/application.ts
@@ -1,8 +1,10 @@
 export class Application {
   // private form: HTMLElement | null = null;
-
+  // 作成中
   start = async (): Promise<void> => {
+
     document.querySelector('form')?.addEventListener('submit', () => {
       document.querySelector('.list-title')!.textContent = "makocchan";
     });
+
     '}';
